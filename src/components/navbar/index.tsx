a29cdcbd70e5dcb332c6727df4b569a01e7b9bc6
mzffrtp <mzffr.tpgl@gmail.com>
2023-06-29 18:50:33 +0200

    responsive design
    media query hook, navbar responsive design, hamburger menu form tailwind  icons

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,9 @@
 import logo from "@/assets/logobody.png";
 import Link from "../links";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { useState } from "react";
+import { Bars3Icon } from "@heroicons/react/20/solid";
 
 type NavbarPropsType = {
   selectedPage: SelectedPage;
@@ -12,6 +15,8 @@ export default function Navbar({
   setSelectedPage,
 }: NavbarPropsType) {
   const flexBetween = "flex items-center justify-between";
+  const isAboveMediumScreens = useMediaQuery("(min-width:1060px");
+  const [menuToogled, setMenuToogled] = useState<boolean>(false);
   return (
     <nav>
       <div
@@ -20,43 +25,55 @@ export default function Navbar({
         <div className={` ${flexBetween}  mx-auto w-5/6`}>
           <div className={` ${flexBetween}  w-full gap-16`}>
             {/* Left side */}
-            <div className={`${flexBetween} w-1/5 `}>
+            <div className={`${flexBetween} text-xl `}>
               <img src={logo} />
               <p className="text-center w-56"> MT Body Worx</p>
             </div>
             {/* Middle and Right side */}
-            <div className={`${flexBetween} w-4/5 py-4`}>
-              <div className={`${flexBetween} gap-16 text-sm`}>
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Benefits"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Our Classes"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Contact Us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+
+            {isAboveMediumScreens ? (
+              <div className={`${flexBetween} w-4/5 py-4`}>
+                <div className={`${flexBetween} gap-16 text-sm`}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween} gap-8`}>
+                  <button className="w-24 hover:bg-fuchsia-200 hover:rounded-full text-lg p-2">
+                    Sign In
+                  </button>
+                  <button className="w-52 rounded-full bg-lime-300 hover:bg-fuchsia-200 text-lg p-2">
+                    Become a member
+                  </button>
+                </div>
               </div>
-              <div className={`${flexBetween} gap-8`}>
-                <button className="w-24 hover:bg-fuchsia-200 hover:rounded-full text-lg p-2">
-                  Sign In
-                </button>
-                <button className="w-52 rounded-full bg-lime-300 hover:bg-fuchsia-200 text-lg p-2">
-                  Become a member
-                </button>
-              </div>
-            </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-500 p-2"
+                onClick={() => {
+                  setMenuToogled(!menuToogled);
+                }}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
         </div>
       </div>
